e1b75f0e2b3596f9a1131a6fc9b9c279bf2d9113
rionmaulanaa2 <rionmaulana2@gmail.com>
2026-02-12 01:36:14 +0700

    Add Riels.lua script

---
+++ scripts/Riels.lua
@@ -0,0 +1,345 @@
+-- TODO: fix auto brainrots ( im not fixing xd )
+repeat print'loading' until game:IsLoaded()
+local p = game:GetService'Players'
+local reps = game:GetService'ReplicatedStorage'
+local rf = reps.RemoteFunctions
+local lp = p.LocalPlayer
+local function godmode()
+ local at = workspace.ActiveTsunamis
+ if not at then return end
+ for i, v in pairs(at:GetDescendants()) do
+  if v:IsA'BasePart' and v.Name == 'Hitbox' then
+   v:Destroy()
+  end
+ end
+end
+local function notsu()
+ local at = workspace.ActiveTsunamis
+ if not at then return end
+ for i, v in pairs(at:GetDescendants()) do
+  if v:IsA'BasePart' then
+   v.Transparency = 1
+  end
+ end
+end
+local function upgradespeed()
+ local remote = rf.UpgradeSpeed
+ remote:InvokeServer(5)
+end
+local function rebirth()
+ local remote = rf.Rebirth
+ remote:InvokeServer()
+end
+local function upgrade()
+ local remote = rf.UpgradeBase
+ remote:InvokeServer()
+end
+local function upgradec()
+ local remote = rf.UpgradeCarry
+ remote:InvokeServer()
+end
+local function sellall()
+ local remote = rf.SellAll
+ remote:InvokeServer()
+end
+local function upgradeb(slot)
+ --"Slot1"
+ local remote = rf.UpgradeBrainrot
+ remote:InvokeServer(slot)
+end
+local function getbase()
+ local bases = workspace.Bases
+ for i, v in pairs(bases:GetChildren()) do
+  if v:IsA'Model' and v:GetAttribute'Holder' == lp.UserId then
+   return v
+  end
+ end
+ return nil
+end
+local function upgradeallb()
+ local base = getbase()
+ if not base then print'nobase'return end
+ for i, v in pairs(base.Slots:GetChildren()) do
+  if v:IsA'Model' and v.Name:lower():find'slot' and v:FindFirstChildWhichIsA'Tool' then
+   upgradeb(v.Name)
+  end
+ end
+end
+local function home()
+ local base = getbase()
+ if not base then return end
+ local part = base.Home
+ if not part then return end
+ pcall(function()
+  game:GetService'Players'.LocalPlayer.Character.HumanoidRootPart.CFrame = part.CFrame
+ end)
+end
+local function speedchanger()
+ local speed = getgenv().Scv or 16
+ lp:SetAttribute('CurrentSpeed', speed)
+end
+local tier = ''
+local function gayfunc()
+ local plrgui = lp.PlayerGui
+ if not plrgui then return end
+ for i, v in pairs(plrgui:GetDescendants()) do
+  if v:IsA'TextLabel' and v.Text == 'Carry limit reached!' then
+   local safearea = workspace.Misc.Ground
+   game:GetService'Players'.LocalPlayer.Character.HumanoidRootPart.CFrame = safearea.CFrame + CFrame.new(0,5,0)
+  end
+ end
+end
+local function autobrainrot()
+ if not tier or tier == '' then print'line 84'return end
+ local ab = workspace.ActiveBrainrots
+ if not ab then print'line 85'return end
+ local folder = ab:FindFirstChild(tier)
+ if not folder then print'line 87'return end
+ for i, v in pairs(folder:GetChildren()) do
+  if v:IsA'Model' and not v:GetAttribute'Fahh' then
+   v:SetAttribute('Fahh','dih')
+   pcall(function()
+    game:GetService'Players'.LocalPlayer.Character.HumanoidRootPart.CFrame = v.PrimaryPart.CFrame
+   end)
+   task.wait(1)
+   local handle = v:FindFirstChild'Handle'
+   if handle then
+    local prompt = handle.TakePrompt
+    if prompt then
+     pcall(function()
+      task.wait(1)
+      fireproximityprompt(prompt)
+     end)
+    end
+   end
+  end
+ end
+end
+local function gettiers()
+ local tiers = {}
+ local ab = workspace.ActiveBrainrots
+ if not ab then return end
+ for i, v in pairs(ab:GetChildren()) do
+  if v:IsA'Folder' then
+   table.insert(tiers, v.Name)
+  end
+ end
+ return tiers
+end
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+WindUI:SetNotificationLower(true)
+local Window = WindUI:CreateWindow({
+ Title = "Escape Tsumami For Brainrots!",
+ Icon = "sparkles",
+ Author = "by luau.org",
+})
+Window:SetToggleKey(Enum.KeyCode.L)
+local Main = Window:Tab({
+ Title = "Main",
+ Icon = "sparkles",
+ Locked = false,
+})
+local AutoB = Window:Tab({
+ Title = "Auto Brainrots",
+ Icon = "sparkles",
+ Locked = false,
+})
+Main:Select()
+local Tth = Main:Button({
+ Title = "Tp to home",
+ Locked = false,
+ Callback = function()
+  pcall(function()
+   home()
+  end)
+ end
+})
+local Gm = Main:Toggle({
+ Title = "Godmode",
+ Icon = "sparkles",
+ Type = "Toggle",
+ Value = false,
+ Callback = function(state)
+  getgenv().Gm = state
+ end
+})
+local Nt = Main:Toggle({
+ Title = "No tsumani [ u can still die ]",
+ Icon = "sparkles",
+ Type = "Toggle",
+ Value = false,
+ Callback = function(state)
+  getgenv().Nt = state
+ end
+})
+local Aus = Main:Toggle({
+ Title = "Auto upgrade speed",
+ Icon = "sparkles",
+ Type = "Toggle",
+ Value = false,
+ Callback = function(state)
+  getgenv().Aus = state
+ end
+})
+local Auc = Main:Toggle({
+ Title = "Auto upgrade carry",
+ Icon = "sparkles",
+ Type = "Toggle",
+ Value = false,
+ Callback = function(state)
+  getgenv().Auc = state
+ end
+})
+local Asa = Main:Toggle({
+ Title = "Auto sell all",
+ Icon = "sparkles",
+ Type = "Toggle",
+ Value = false,
+ Callback = function(state)
+  getgenv().Asa = state
+ end
+})
+local Ar = Main:Toggle({
+ Title = "Auto rebirth",
+ Icon = "sparkles",
+ Type = "Toggle",
+ Value = false,
+ Callback = function(state)
+  getgenv().Ar = state
+ end
+})
+local Aub = Main:Toggle({
+ Title = "Auto upgrade base",
+ Icon = "sparkles",
+ Type = "Toggle",
+ Value = false,
+ Callback = function(state)
+  getgenv().Aub = state
+ end
+})
+local Aub2 = Main:Toggle({
+ Title = "Auto upgrade brainrots",
+ Icon = "sparkles",
+ Type = "Toggle",
+ Value = false,
+ Callback = function(state)
+  getgenv().Aub2 = state
+ end
+})
+local Sc = Main:Toggle({
+ Title = "Speed changer",
+ Icon = "sparkles",
+ Type = "Toggle",
+ Value = false,
+ Callback = function(state)
+  getgenv().Sc = state
+ end
+})
+local Scv = Main:Slider({
+ Title = "Speed",
+ Step = 1,
+ Value = {
+  Min = 16,
+  Max = 300,
+  Default = 16,
+ },
+ Callback = function(value)
+  getgenv().Scv = tonumber(value) or 16
+ end
+})
+local AB = AutoB:Toggle({
+ Title = "Auto brainrots",
+ Icon = "sparkles",
+ Type = "Toggle",
+ Value = false,
+ Callback = function(state)
+  getgenv().AutoBrainrots = state
+ end
+})
+local Tiers = AutoB:Dropdown({
+ Title = "Tier",
+ Values = gettiers(),
+ Value = "Secret",
+ Callback = function(option) 
+  tier = tostring(option)
+  print(tier)
+ end
+})
+spawn(function()
+ while true do
+  task.wait()
+  if getgenv().Gm then
+   spawn(function()
+    pcall(function()
+     godmode()
+    end)
+   end)
+  end
+  if getgenv().Nt then
+   spawn(function()
+    pcall(function()
+     notsu()
+    end)
+   end)
+  end
+  if getgenv().Aus then
+   spawn(function()
+    pcall(function()
+     upgradespeed()
+    end)
+   end)
+  end
+  if getgenv().Auc then
+   spawn(function()
+    pcall(function()
+     upgradec()
+    end)
+   end)
+  end
+  if getgenv().Asa then
+   spawn(function()
+    pcall(function()
+     sellall()
+    end)
+   end)
+  end
+  if getgenv().Ar then
+   spawn(function()
+    pcall(function()
+     rebirth()
+    end)
+   end)
+  end
+  if getgenv().Aub then
+   spawn(function()
+    pcall(function()
+     upgrade()
+    end)
+   end)
+  end
+  if getgenv().Aub2 then
+   spawn(function()
+    pcall(function()
+     upgradeallb()
+    end)
+   end)
+  end
+  if getgenv().Sc then
+   spawn(function()
+    pcall(function()
+     speedchanger()
+    end)
+   end)
+  end
+  if getgenv().AutoBrainrots then
+   spawn(function()
+    pcall(function()
+     spawn(godmode)
+     spawn(notsu)
+     spawn(gayfunc)
+     spawn(autobrainrot)
+    end)
+   end)
+  end
+ end
+end)
